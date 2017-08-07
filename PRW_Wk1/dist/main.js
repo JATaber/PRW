2049b98effb59a78f7609534bb932bca74f38b48
James Taber <JATaber@fullsail.edu>
2017-08-06 21:56:41 -0500

    got delete to work
    got the delete functionality to work

--- PRW_Wk1/dist/main.js
+++ PRW_Wk1/dist/main.js
@@ -23,19 +23,33 @@ $(document).ready(function () {
     for (x = 0; x < data.recipes.length; x++) {
         var rating = data.recipes[x].starRating;
         var desc = data.recipes[x].description;
+        var id = data.recipes[x].recipeID;
 
-        recData += '<article >';
+        recData += '<article data-id="' + id + '">';
         recData += '<h2>' + data.recipes[x].category + '</h2>';
         recData += '<img src=' + data.recipes[x].photoUrl + ' alt="recipe-image">';
         recData += '<h3>' + data.recipes[x].title + '</h3>';
         recData += '<p>' + desc + '</p>';
-        recData += '<div class="artFoot"><p>Star Rating: ' + rating.toFixed(1) + '</p><p><i class="fa fa-pencil" aria-hidden="true">' + '</i> <i class="fa fa-trash" aria-hidden="true"></i></p></div>';
+        recData += '<div class="artFoot"><p>Star Rating: ' + rating.toFixed(1) + '</p><p><button class="edit-btn" id="edit-btn">' + '<i class="fa fa-pencil fa-fw" aria-hidden="true">' + '</i></button><button class="del-btn" id="del-btn">' + '<i class="fa fa-trash fa-fw" id="' + x + '" aria-hidden="true"></i></button></p></div>';
         recData += '</article>';
+        console.log(id);
     }
 
     $('#recipe-details').append(recData);
 
-    var remove = $('i.fa-trash');
+    $('.fa-trash').on('click', function (e) {
+        var trashID = e.target.id;
+
+        $(this).parent().parent().parent().parent().remove();
+        console.log(trashID);
+    });
+    /*
+    const remove = $('#del-btn');
+     remove.on('click', function(){
+        remove.parent().parent().parent().remove();
+        console.log(remove.parent().parent().parent());
+    });
+    */
 
     var edit = $('i.fa-pencil');
 });
