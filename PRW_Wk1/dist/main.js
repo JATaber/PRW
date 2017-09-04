060a26458a788d4c7ae46beba8b40eed73820d0d
James Taber <JATaber@fullsail.edu>
2017-09-03 22:24:02 -0500

    ran build

--- PRW_Wk1/dist/main.js
+++ PRW_Wk1/dist/main.js
@@ -1,34 +1,63 @@
 'use strict';
 
 $(document).ready(function () {
-    var data = $.get({
-        url: 'https://api.myjson.com/bins/f4ayd',
-        dataType: 'json',
-        async: false
-    }).responseJSON;
 
-    console.log(data.recipes);
+    var storage = window.localStorage;
+    var dataString = void 0;
 
+    if (!storage.recipes) {
+        var storageData = $.get({
+            url: 'https://api.myjson.com/bins/f4ayd',
+            dataType: 'json',
+            async: false
+        }).responseJSON;
+
+        console.log(storageData);
+
+        dataString = JSON.stringify(storageData);
+        console.log(dataString);
+
+        storage.setItem("recipes", dataString);
+
+        console.log(JSON.parse(storage.getItem("recipes")));
+    } else {
+        var _data = JSON.parse(storage.getItem("recipes"));
+
+        console.log(_data);
+    }
+
+    var data = JSON.parse(storage.getItem("recipes"));
     var modalAdd = $('#addRecipe');
-
     var addBtn = $('#add');
+    var closeModal = $('.fa-times');
+    var cancelBtn = $('.btn-cancel');
+    var submitBtn = $('.btn-submit');
 
-    addBtn.on('click', function (e) {
+    addBtn.on('click', function () {
+        modalAdd.toggle();
+    });
+
+    closeModal.on('click', function (e) {
         e.preventDefault();
-        modalAdd.show();
+        modalAdd.hide();
+    });
+
+    cancelBtn.on('click', function (e) {
+        e.preventDefault();
+        modalAdd.hide();
     });
 
     var recData = '';
 
-    for (x = 0; x < data.recipes.length; x++) {
-        var rating = data.recipes[x].starRating;
-        var desc = data.recipes[x].description;
-        var id = data.recipes[x].recipeID;
+    for (x = 0; x < data.length; x++) {
+        var rating = data[x].starRating;
+        var desc = data[x].description;
+        var id = data[x].recipeID;
 
         recData += '<article data-id="' + id + '">';
-        recData += '<h2>' + data.recipes[x].category + '</h2>';
+        recData += '<h2>' + data[x].category + '</h2>';
         recData += '<img src="images/food-salad-restaurant-person.jpg" alt="recipe-image">';
-        recData += '<h3>' + data.recipes[x].title + '</h3>';
+        recData += '<h3>' + data[x].title + '</h3>';
         recData += '<p>' + desc + '</p>';
         recData += '<div class="artFoot"><p>Star Rating: ' + rating.toFixed(1) + '</p><p><button class="edit-btn" id="edit-btn">' + '<i class="fa fa-pencil fa-fw" aria-hidden="true">' + '</i></button><button class="del-btn" id="del-btn">' + '<i class="fa fa-trash fa-fw" id="' + x + '" aria-hidden="true"></i></button></p></div>';
         recData += '</article>';
@@ -37,6 +66,35 @@ $(document).ready(function () {
 
     $('#recipe-details').append(recData);
 
+    submitBtn.on('click', function (e) {
+        e.preventDefault();
+
+        var articleId = data.length += 1;
+        var arrayID = articleId -= 1;
+        var dish = $('#dishType').val();
+        var title = $('#name').val();
+        var descrip = $('#details').val();
+
+        data.push({ recipeID: articleId, title: title, description: descrip, category: dish, starRating: 0, photoUrl: null });
+
+        data = JSON.parse(storage.getItem("recipes"));
+        console.log(data);
+
+        recData = '';
+
+        recData += '<article data-id="' + articleId + '">';
+        recData += '<h2>' + dish + '</h2>';
+        recData += '<img src="images/food-salad-restaurant-person.jpg" alt="recipe-image">';
+        recData += '<h3>' + title + '</h3>';
+        recData += '<p>' + descrip + '</p>';
+        recData += '<div class="artFoot"><p>Star Rating: 0</p><p><button class="edit-btn" id="edit-btn">' + '<i class="fa fa-pencil fa-fw" aria-hidden="true">' + '</i></button><button class="del-btn" id="del-btn">' + '<i class="fa fa-trash fa-fw" id="' + arrayID + '" aria-hidden="true"></i></button></p></div>';
+        recData += '</article>';
+        console.log(arrayID);
+
+        $('#recipe-details').append(recData);
+        modalAdd.hide();
+    });
+
     $('.fa-trash').on('click', function (e) {
         var trashID = e.target.id;
 
@@ -44,13 +102,14 @@ $(document).ready(function () {
         console.log(trashID);
     });
 
-    /*
-    const remove = $('#del-btn');
-     remove.on('click', function(){
-        remove.parent().parent().parent().remove();
-        console.log(remove.parent().parent().parent());
-    });
-    */
-
     var edit = $('i.fa-pencil');
+
+    edit.on('click', function (e) {
+        var editId = e.target.id;
+
+        $('#dishType').val();
+
+        modalAdd.toggle();
+        //$(this).parent().parent().parent().parent().remove();
+    });
 });
